47c150eb5b8bdb53f73caf593a7557ed878e2185
Maddie Revill <maddierevill@MadRev-Air.local>
2016-11-17 21:02:01 -0800

    Implement Queue class

--- w1d5/diy_adts.rb
+++ w1d5/diy_adts.rb
@@ -15,3 +15,21 @@ class Stack
     @els.dup
   end
 end
+
+class Queue
+  def initialize
+    @els = []
+  end
+
+  def enqueue(el)
+    @els.push(el)
+  end
+
+  def dequeue
+    @els.shift
+  end
+
+  def show
+    @els.dup
+  end
+end
