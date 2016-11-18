6d66e4042a129a1c68eddd70abe1d21d3b64ace4
Maddie Revill <maddierevill@MadRev-Air.local>
2016-11-17 22:12:11 -0800

    Implement Map class

--- w1d5/diy_adts.rb
+++ w1d5/diy_adts.rb
@@ -33,3 +33,28 @@ class Queue
     @els.dup
   end
 end
+
+class Map
+  def initialize
+    @pairs = Array.new
+  end
+
+
+  def lookup(key)
+    @pairs.find{|pair| pair[0] == key}[1]
+  end
+
+  def remove(key)
+    @pairs.delete_if {|pair| pair[0] == key}
+  end
+
+  def assign(key, value)
+    self.remove(key)
+    @pairs << [key, value]
+  end
+
+  def show
+    @pairs.dup
+  end
+
+end
